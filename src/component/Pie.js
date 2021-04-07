92f7a0e2a415b92a6b7786be09db6e596977ba31
Lendaf <eric.li@epitech.eu>
2021-04-07 19:02:43 +0200

    [ADD] multiple level pie chart

--- src/component/Pie.js
+++ src/component/Pie.js
@@ -1,149 +1,56 @@
 /* Pie.js */
 import React, { useState } from 'react'
-import { Sunburst } from 'react-vis'
+import { PieChart, Pie, Cell } from "recharts";
+import data from "../data/test.json"
 
-var newChild = [
-	{
-		id: "00",
-		name: 'Poste 1',
-		value: 100,
-		color: '#30132a',
-		children: [],
-	},
-	{
-		id: "01",
-		name: 'Poste 2',
-		value: 256,
-		color: '#666d40',
-		children: [],
-	},
-	{
-		id: "02",
-		name: 'Poste 3',
-		value: 32,
-		color: '#632607',
-		children: [],
-	},
-	{
-		id: "03",
-		name: 'Poste 4',
-		value: 56,
-		color: '#e0e57c',
-		children: [],
-	},
-	{
-		id: "04",
-		name: 'Poste 5',
-		value: 89,
-		color: '#d87ec5',
-		children: [],
-	},
-]
+const Camembert = () => {
+	const [Data01] = useState(data.firstLayer.data)
+	const [Data02, setData02] = useState([])
+	const [Data03, setData03] = useState([])
+	const [Label, setLabel] = useState([true, false, false])
+	const [selected, setSelected] = useState(["", "", ""])
 
-var anotherChild = [
-	{
-		id: "000",
-		name: 'Poste 1',
-		value: 100,
-		color: '#30132a',
-		children: [],
-	},
-	{
-		id: "001",
-		name: 'Poste 2',
-		value: 256,
-		color: '#666d40',
-		children: [],
-	},
-	{
-		id: "002",
-		name: 'Poste 3',
-		value: 32,
-		color: '#632607',
-		children: [],
-	},
-	{
-		id: "003",
-		name: 'Poste 4',
-		value: 56,
-		color: '#e0e57c',
-		children: [],
-	},
-	{
-		id: "004",
-		name: 'Poste 5',
-		value: 89,
-		color: '#d87ec5',
-		children: [],
-	},
-]
-
-var Data = {
-	children: [
-		{
-			id: "0",
-			name: 'Poste 1',
-			value: 100,
-			color: '#30132a',
-			children: [],
-		},
-		{
-			id: "1",
-			name: 'Poste 2',
-			value: 256,
-			color: '#666d40',
-			children: [],
-		},
-		{
-			id: "2",
-			name: 'Poste 3',
-			value: 32,
-			color: '#632607',
-			children: [],
-		},
-		{
-			id: "3",
-			name: 'Poste 4',
-			value: 56,
-			color: '#e0e57c',
-			children: [],
-		},
-		{
-			id: "4",
-			name: 'Poste 5',
-			value: 89,
-			color: '#d87ec5',
-			children: [],
-		},
-	]
-}
-
-const Pie = () => {
-	const [data, setdata] = useState(Data)
-
-	function differentLayer(d) {
-		var tmp = data
-		if ((d.id).length === 1) {
-			tmp.children[(d.id)[0]].children = newChild
-			setdata(Data)
-		} else if ((d.id).length === 2) {
-			tmp.children[(d.id)[0]].children[(d.id)[1]].children = anotherChild
-			setdata(Data)
-		}
-		console.log(data)
+	function addNewLayer(setState, newSelected, newData, newLabel) {
+		setState(newData)
+		setLabel(newLabel)
+		setSelected(newSelected)
 	}
 
 	return (
-		<Sunburst
-			data={data}
-			getLabel={d => d.name}
-			showLabels
-			getSize={d => d.value}
-			width={400}
-			height={300}
-			onValueClick={(d, _) => differentLayer(d)}
-		/>
+		<PieChart width={400} height={400}>
+			<Pie
+				data={Data01}
+				outerRadius={70}
+				label={Label[0]}
+				onClick={(d) => addNewLayer(setData02, ["", d.name, selected[2]], data.secondLayer[d.name].data, [false, true, false])}
+			>
+				{Data01.map((entry, index) => (
+          			<Cell key={`cell-${index}`} fill={data.firstLayer.color[index]} />
+        		))}
+			</Pie>
+			<Pie
+				data={Data02}
+				outerRadius={100}
+				label={Label[1]}
+				onClick={(d) => addNewLayer(setData03, ["", selected[1], d.name], data.thirdLayer[d.name].data, [false, false, true])}
+			>
+				{Data02.map((entry, index) => (
+          			<Cell key={`cell-${index}`} fill={data.secondLayer[selected[1]].color[index]} />
+        		))}
+
+			</Pie>
+			<Pie
+				data={Data03}
+				innerRadius={110}
+				outerRadius={130}
+				label={Label[2]}
+			>
+				{Data03.map((entry, index) => (
+          			<Cell key={`cell-${index}`} fill={data.thirdLayer[selected[2]].color[index]} />
+        		))}
+			</Pie>
+		</PieChart>
 	)
 }
 
-export default Pie;
+export default Camembert;
