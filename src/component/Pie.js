6818d8537abc499c7f95ae4a8328c278f999c359
Lendaf <eric.li@epitech.eu>
2021-04-08 12:01:31 +0200

    [FIX] bad end angle when deleting data

--- src/component/Pie.js
+++ src/component/Pie.js
@@ -17,9 +17,8 @@ const Camembert = () => {
 		setOldData(newData)
 	}
 
-	function addNewLayer(setNewData, newSelected, newData, newLabel, setOldData, oldData, d) {
+	function addNewLayer(setNewData, newSelected, newData, setOldData, oldData, d) {
 		setNewData(newData)
-		setLabel(newLabel)
 		setSelected(newSelected)
 		remove(setOldData, d.name, oldData)
 	}
@@ -29,30 +28,28 @@ const Camembert = () => {
 			<Pie
 				data={Data01}
 				startAngle={selected[1].endAngle ? selected[1].endAngle : 0}
-				endAngle={selected[1].startAngle ? selected[1].startAngle : 360}
+				endAngle={selected[1].startAngle ? 360 + selected[1].startAngle : 360}
 				outerRadius={100}
-				label={Label[0]}
-				cx={200}
-				cy={200}
-				onClick={(d) => addNewLayer(setData02, [{}, d, selected[2]], data.secondLayer[d.name].data, [false, true, false], setData01, Data01, d)}
-				onMouseOver={() => console.log(selected)}
+				label
+				onClick={(d) => addNewLayer(setData02, [{}, d, selected[2]], data.secondLayer[d.name].data, setData01, Data01, d)}
+				onMouseOver={() => console.log(Object.entries(selected[1]).length === 0)}
 			>
 				{Data01.map((entry, index) => (
-					<Cell key={`cell-${index}`} strokeWidth={0} fill={data.firstLayer.color[index + (Label[0] ? 0 : 1)]} />
+					Object.entries(selected[1]).length === 0 ? <Cell key={`cell-${index}`} fill={data.firstLayer.color[index]} /> :
+					<Cell key={`cell-${index}`} strokeWidth={0} fill={data.firstLayer.color[index + 1]} />
         		))}
 			</Pie>
 			<Pie
 				data={Data02}
-				startAngle={selected[1].startAngle + 5}
-				endAngle={selected[1].endAngle - 5}
+				startAngle={selected[1].startAngle}
+				endAngle={selected[1].endAngle}
 				outerRadius={130}
-				label={Label[1]}
-				cx={205}
-				cy={195}
-				onClick={(d) => addNewLayer(setData03, [{}, selected[1], d], data.thirdLayer[d.name].data, [false, false, true], setData02, Data02, d)}
+				label
+				onClick={(d) => addNewLayer(setData03, [{}, selected[1], d], data.thirdLayer[d.name].data, setData02, Data02, d)}
 			>
 				{Data02.map((entry, index) => (
-					<Cell key={`cell-${index}`} strokeWidth={0} fill={data.secondLayer[selected[1].name].color[index + (Label[1] ? 0 : 1)]} />
+					Object.entries(selected[2]).length === 0 ? <Cell key={`cell-${index}`} fill={data.secondLayer[selected[1].name].color[index]} /> :
+					<Cell key={`cell-${index}`} strokeWidth={0} fill={data.secondLayer[selected[1].name].color[index + 1]} />
         		))}
 			</Pie>
 			<Pie
@@ -60,12 +57,10 @@ const Camembert = () => {
 				startAngle={selected[2].startAngle}
 				endAngle={selected[2].endAngle}
 				outerRadius={160}
-				cx={210}
-				cy={196}
-				label={Label[2]}
+				label
 			>
 				{Data03.map((entry, index) => (
-          			<Cell key={`cell-${index}`} strokeWidth={0} fill={data.thirdLayer[selected[2].name].color[index]} />
+          			<Cell key={`cell-${index}`} fill={data.thirdLayer[selected[2].name].color[index]} />
         		))}
 			</Pie>
 		</PieChart>
